49913788ca08e6a6b95e173dec0038bd0c3c76dd
Prerak Mathur <mathur.prerak@gmail.com>
2021-10-04 03:40:59 +0530

    Solution to LargestNumber

---
+++ Java/LargestNumber.java
@@ -0,0 +1,12 @@
+// Solution to https://leetcode.com/problems/largest-number/
+
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+        String largestNum = Arrays.stream(nums)
+            .boxed()
+            .map(String::valueOf)
+            .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
+            .collect(Collectors.joining(""));
+        return largestNum.charAt(0) == '0' ? "0" : largestNum;
+      }
+}
